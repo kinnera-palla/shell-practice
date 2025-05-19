363803d7ee6736232c0cca92a895f429419da723
kinnera-palla <pallakinnera19@gmail.com>
2025-05-19 12:11:54 -0700

    01-helloworld

--- 08-specialvars.sh
+++ 08-specialvars.sh
@@ -2,3 +2,10 @@
 
 echo "all variables passed to script: $@:"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "current dir: $PWD"
+echo "user runnins: $USER"
+echo "Home dir: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of lat sommand: $!"
