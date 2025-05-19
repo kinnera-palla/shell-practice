fdf047beb9f0818f1ad4378a95b83fc03445efc3
kinnera-palla <pallakinnera19@gmail.com>
2025-05-19 16:39:07 -0700

    01-helloworld

--- 14.logs.sh
+++ 14.logs.sh
@@ -11,24 +11,24 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo " script started executing at: $(date)" &>>$LOG_FILE
+echo " script started executing at: $(date)" | tee -a $LOG_FILE
 
 
 if [ $USERID -ne 0 ]
 then
-    echo  -e " $R ERROR:: Please run this script with root access $N"
+    echo  -e " $R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto 127
 else
-    echo "You are running with root access"
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo "Installing $2 is ... $G SUCCESS $N"
+        echo "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo "Installing $2 is ... $R FAILURE $N"
+        echo "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -36,7 +36,7 @@ VALIDATE(){
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed... going to install it"
+    echo "MySQL is not installed... going to install it" | tee -a $LOG_FILE
     dnf install mysql -y
     VALIDATE $? "MySQL"
 else
@@ -46,7 +46,7 @@ fi
 dnf list installed python3
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed... going to install it"
+    echo "python3 is not installed... going to install it" | tee -a $LOG_FILE
     dnf install python3 -y
     VALIDATE $? "python3"
 else
